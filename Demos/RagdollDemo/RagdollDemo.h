2d722e51d58069397c08c62d5fd82fa21b14d137
ReemAlattas <ralattas@live.com>
2016-08-22 14:04:37 -0400

    Assignment 6 - Step 6

--- Demos/RagdollDemo/RagdollDemo.h
+++ Demos/RagdollDemo/RagdollDemo.h
@@ -80,6 +80,11 @@ public:
     
     void CreateHinge(int index, int body1, int body2, double x, double y, double z, double ax, double ay, double az);
     
+    btVector3 PointWorldToLocal(int index, btVector3 &p) {
+        btTransform local1 = body[index]->getCenterOfMassTransform().inverse();
+        return local1 * p;
+    }
+    
     void DeleteObject(int index);
     
 	static DemoApplication* Create()
